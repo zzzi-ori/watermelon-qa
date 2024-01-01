f2dcfdefde0c8346cd0028063da7c8855dd0d989
da-in <talown@naver.com>
2024-01-01 15:42:11 +0900

    fix block size

---
+++ src/pages/play/setting.ts
@@ -0,0 +1,18 @@
+type Block = {
+    size: number;
+};
+
+// const score = [3, 5, 7, 10, 15, 20, 25, 30, 40, 50, 100]
+
+export const blocks: { [key: number]: Block } = {
+    1: { size: 75 },
+    2: { size: 98 },
+    3: { size: 137 },
+    4: { size: 180 },
+    5: { size: 200 },
+    6: { size: 240 },
+    7: { size: 280 },
+    8: { size: 320 },
+    9: { size: 320 },
+    10: { size: 400 },
+};
