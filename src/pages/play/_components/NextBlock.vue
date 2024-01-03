b160a6d24702db39ef51e1d728c4f5208c6e9339
da-in <da-in@wavve.com>
2024-01-03 21:14:45 +0900

    create next block indicator

---
+++ src/pages/play/_components/NextBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center bg-[#FCBF31] rounded-full w-36 pl-10 -ml-6 border-2 border-black">
+      <span class="text-4xl mr-3" >Next</span>
+      <div :style="{background: block.color}" class="w-[26px] h-[26px] rounded-full border-2 border-black"></div>
+  </div>
+</template>
+<script setup lang="ts">
+import {computed} from "vue";
+import {blocks} from "../setting.ts";
+
+const props = defineProps({
+  nextIndex: Number
+})
+
+const block = computed(() => blocks[props.nextIndex])
+</script>
