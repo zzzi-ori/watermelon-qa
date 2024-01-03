672aef60e729a9f37fb43464e0989b5f730ae061
da-in <da-in@wavve.com>
2024-01-03 22:41:46 +0900

    preview next block

--- src/pages/play/_components/NextBlock.vue
+++ src/pages/play/_components/NextBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center bg-[#FCBF31] rounded-full w-36 pl-10 -ml-6 border-2 border-black">
       <span class="text-4xl mr-3" >Next</span>
-      <div :style="{background: block.color}" class="w-[26px] h-[26px] rounded-full border-2 border-black"></div>
+      <div :style="{background: block?.color}" class="w-[26px] h-[26px] rounded-full border-2 border-black"></div>
   </div>
 </template>
 <script setup lang="ts">
