1ffdba478a5286315e5f3d9bd3b2a3dfda1dabb1
da-in <da-in@wavve.com>
2024-01-03 21:55:28 +0900

    create game over

--- src/pages/play/setting.ts
+++ src/pages/play/setting.ts
@@ -1,18 +1,16 @@
 type Block = {
     size: number;
+    color: string;
 };
-
-// const score = [3, 5, 7, 10, 15, 20, 25, 30, 40, 50, 100]
-
 export const blocks: { [key: number]: Block } = {
-    1: { size: 75 },
-    2: { size: 98 },
-    3: { size: 137 },
-    4: { size: 180 },
-    5: { size: 200 },
-    6: { size: 240 },
-    7: { size: 280 },
-    8: { size: 320 },
-    9: { size: 320 },
-    10: { size: 400 },
+    1: { size: 26, color: '#E573D2' },
+    2: { size: 40, color: '#F69B30' },
+    3: { size: 54, color: '#41B5BD' },
+    4: { size: 90, color: '#6079FF' },
+    5: { size: 100, color: '#FEDC2D' },
+    6: { size: 120, color: '#FD9D0E' },
+    7: { size: 140, color: '#CCF576' },
+    8: { size: 160, color: '#FC6B6B' },
+    9: { size: 160, color: '#BB4BFF' },
+    10: { size: 200, color: '#3BE42D' },
 };
