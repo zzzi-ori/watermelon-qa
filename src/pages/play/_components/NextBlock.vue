0fb91e8924fc3690bf607349ae18d85d55793dc9
da-in <da-in@wavve.com>
2024-01-20 16:45:13 +0900

    add next block image

--- src/pages/play/_components/NextBlock.vue
+++ src/pages/play/_components/NextBlock.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex justify-center items-center bg-[#FCBF31] rounded-[10px] w-28 border-2 border-black">
+  <div class="flex justify-center items-center bg-[#FCBF31] rounded-[10px] w-28 py-1 border-2 border-black">
       <span class="text-[18px] mr-3 font-bold" >NEXT</span>
-      <div :style="{background: block?.color}" class="w-[26px] h-[26px] rounded-full border-2 border-black"></div>
+      <img :src="block" class="w-[26px] h-[26px]" />
   </div>
 </template>
 <script setup lang="ts">
 import {computed} from "vue";
-import {blocks} from "../setting.ts";
+import {n1, n2, n3, n4, n5, n6, n7, n8, n9, n10} from '/src/assets/next'
 
 const props = defineProps({
   nextIndex: {
@@ -15,5 +15,22 @@ const props = defineProps({
   }
 })
 
-const block = computed(() => blocks[props.nextIndex])
+const Block : {[key:number]: any}  = {
+  1: n1,
+  2: n2,
+  3: n3,
+  4: n4,
+  5: n5,
+  6: n6,
+  7: n7,
+  8: n8,
+  9: n9,
+  10: n10,
+}
+
+const block = computed(() => {
+  if(props.nextIndex in Block){
+    return Block[props.nextIndex]
+  }
+})
 </script>
