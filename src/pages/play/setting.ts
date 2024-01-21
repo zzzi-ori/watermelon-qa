1e70f56dcd3d344ea854206733912d96b997b3cf
da-in <da-in@wavve.com>
2024-01-21 13:57:14 +0900

    fix size and color

--- src/pages/play/setting.ts
+++ src/pages/play/setting.ts
@@ -18,17 +18,30 @@ type Block = {
 const lineColor = '#1E1E1E'
 
 const blocks: { [key: number]: Block } = {
-    1: { size: 26, color: '#FFB7E7', score: 2},
-    2: { size: 40, color: '#FFC8B7', score: 4},
-    3: { size: 54, color: '#FFDB98', score: 6},
-    4: { size: 90, color: '#97FFC6', score: 8},
-    5: { size: 100, color: '#94E9F6', score: 16},
-    6: { size: 120, color: '#FF8FDA', score: 32},
-    7: { size: 140, color: '#FF9A7A', score: 64},
-    8: { size: 160, color: '#FFC964', score: 128},
-    9: { size: 160, color: '#43F091', score: 256},
-    10: { size: 200, color: '#60C7FF', score: 512},
+    1: { size: 30, color: '#94E9F6', score: 2},
+    2: { size: 40, color: '#97FFC6', score: 4},
+    3: { size: 55, color: '#FFB7E7', score: 6},
+    4: { size: 70, color: '#FFC8B7', score: 8},
+    5: { size: 90, color: '#FFDB98', score: 16},
+    6: { size: 110, color: '#60C7FF', score: 32},
+    7: { size: 125, color: '#43F091', score: 64},
+    8: { size: 140, color: '#FF8FDA', score: 128},
+    9: { size: 150, color: '#FF9A7A', score: 256},
+    10: { size: 160, color: '#FFC964', score: 512},
 };
+
+// const blocks: { [key: number]: Block } = {
+//     1: { size: 26, color: '#FFB7E7', score: 2},
+//     2: { size: 40, color: '#FFC8B7', score: 4},
+//     3: { size: 54, color: '#FFDB98', score: 6},
+//     4: { size: 90, color: '#97FFC6', score: 8},
+//     5: { size: 100, color: '#94E9F6', score: 16},
+//     6: { size: 120, color: '#FF8FDA', score: 32},
+//     7: { size: 140, color: '#FF9A7A', score: 64},
+//     8: { size: 160, color: '#FFC964', score: 128},
+//     9: { size: 160, color: '#43F091', score: 256},
+//     10: { size: 200, color: '#60C7FF', score: 512},
+// };
 
 export {lineColor, blocks}
 
