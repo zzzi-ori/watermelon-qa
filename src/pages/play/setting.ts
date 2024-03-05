733bcd10e44d47b0a748af11d627a81ae4ac8c09
da-in <talown@naver.com>
2024-03-05 16:16:43 +0900

    fix lint

--- src/pages/play/setting.ts
+++ src/pages/play/setting.ts
@@ -18,17 +18,17 @@ type Block = {
 const lineColor = '#1E1E1E'
 
 const blocks: { [key: number]: Block } = {
-    1: { size: 30, color: '#94E9F6', score: 2},
-    2: { size: 40, color: '#97FFC6', score: 4},
-    3: { size: 55, color: '#FFB7E7', score: 6},
-    4: { size: 70, color: '#FFC8B7', score: 8},
-    5: { size: 90, color: '#FFDB98', score: 16},
-    6: { size: 110, color: '#60C7FF', score: 32},
-    7: { size: 125, color: '#43F091', score: 64},
-    8: { size: 140, color: '#FF8FDA', score: 128},
-    9: { size: 150, color: '#FF9A7A', score: 256},
-    10: { size: 160, color: '#FFC964', score: 512},
-};
+  1: {size: 30, color: '#94E9F6', score: 2},
+  2: {size: 40, color: '#97FFC6', score: 4},
+  3: {size: 55, color: '#FFB7E7', score: 6},
+  4: {size: 70, color: '#FFC8B7', score: 8},
+  5: {size: 90, color: '#FFDB98', score: 16},
+  6: {size: 110, color: '#60C7FF', score: 32},
+  7: {size: 125, color: '#43F091', score: 64},
+  8: {size: 140, color: '#FF8FDA', score: 128},
+  9: {size: 150, color: '#FF9A7A', score: 256},
+  10: {size: 160, color: '#FFC964', score: 512},
+}
 
 // const blocks: { [key: number]: Block } = {
 //     1: { size: 26, color: '#FFB7E7', score: 2},
@@ -45,15 +45,15 @@ const blocks: { [key: number]: Block } = {
 
 export {lineColor, blocks}
 
-export const Background: {[key: number]: any} = {
-    1: b1,
-    2: b2,
-    3: b3,
-    4: b4,
-    5: b5,
-    6: b6,
-    7: b7,
-    8: b8,
-    9: b9,
-    10: b10
+export const Background: { [key: number]: any } = {
+  1: b1,
+  2: b2,
+  3: b3,
+  4: b4,
+  5: b5,
+  6: b6,
+  7: b7,
+  8: b8,
+  9: b9,
+  10: b10
 }
