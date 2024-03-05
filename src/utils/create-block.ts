5ebb28dd21cf14d4e15cf258cfc561c3b450dd63
da-in <da-in@wavve.com>
2024-03-05 16:16:43 +0900

    fix lint

--- src/utils/create-block.ts
+++ src/utils/create-block.ts
@@ -1,22 +1,22 @@
-import {Bodies} from "matter-js"
-import {Background, blocks, lineColor} from "../pages/play/setting.ts"
+import {Bodies} from 'matter-js'
+import {Background, blocks, lineColor} from '../pages/play/setting.ts'
 
-export const createBlock = (index: number, x:number, y:number, isStatic:boolean=false) => {
-    const block = blocks[index]
-    const background = Background[index]
-    const circle = Bodies.circle(x, y, block.size/2, {
-        isStatic,
-        label: String(index),
-        render: {
-            fillStyle: block.color,
-            lineWidth: 2.5,
-            strokeStyle: lineColor,
-            sprite: {
-                texture: background,
-                xScale: 1/3,
-                yScale: 1/3,
-            }
-        }
-    })
-    return circle
+export const createBlock = (index: number, x: number, y: number, isStatic: boolean = false) => {
+  const block = blocks[index]
+  const background = Background[index]
+  const circle = Bodies.circle(x, y, block.size / 2, {
+    isStatic,
+    label: String(index),
+    render: {
+      fillStyle: block.color,
+      lineWidth: 2.5,
+      strokeStyle: lineColor,
+      sprite: {
+        texture: background,
+        xScale: 1 / 3,
+        yScale: 1 / 3,
+      }
+    }
+  })
+  return circle
 }
--- src/utils/get-block-index.ts
+++ src/utils/get-block-index.ts
@@ -2,6 +2,6 @@
  * @param count 몇 번째 블럭인지
  */
 export const getBlockIndex = (count: number) => {
-    const _count = count > 6 ? 6 : count
-    return Math.floor(Math.random() * (_count - 1)) + 1 // 1 ~ count
+  const _count = count > 6 ? 6 : count
+  return Math.floor(Math.random() * (_count - 1)) + 1 // 1 ~ count
 }
