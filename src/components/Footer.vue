d4711db94746051d08f530892c2ed64b48e1d7fe
da-in <da-in@wavve.com>
2024-03-08 22:52:56 +0900

    add github link open in new tab

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-dark-green flex flex-col items-center pb-12 pt-4">
-    <a class="flex items-center gap-1 text-caption-b" href="https://github.com/zzzi-ori">
+    <a class="flex items-center gap-1 text-caption-b" href="https://github.com/zzzi-ori" target="_blank">
       <img class="text-dark-green" :src="github" alt="github"/>
       <h3>zzio-game</h3>
     </a>
