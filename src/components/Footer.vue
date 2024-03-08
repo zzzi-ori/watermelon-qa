576df05e0788b413a20c534ee0c599d29c6b2ffc
da-in <talown@naver.com>
2024-03-08 13:06:57 +0900

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-dark-green flex flex-col items-center pb-12 pt-4">
+    <a class="flex items-center gap-1 text-caption-b" href="https://github.com/zzzi-ori">
+      <img class="text-dark-green" :src="github" alt="github"/>
+      <h3>zzio-game</h3>
+    </a>
+    <span class="text-caption-r">© developers are maybe ok</span>
+  </div>
+</template>
+<script setup lang="ts">
+import github from '../assets/github-green.svg'
+</script>
