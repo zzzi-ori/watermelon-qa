9df6c2faca86bc8d094db21c0b0d5d6402f4c063
da-in <da-in@wavve.com>
2024-03-09 02:51:22 +0900

    update button stroke

--- src/components/button/_assets/LeftLine.vue
+++ src/components/button/_assets/LeftLine.vue
@@ -1,7 +1,7 @@
 <template>
-  <svg width="4" height="24" viewBox="0 0 6 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="6" height="24" viewBox="0 0 6 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-        d="M4.2 1.5C0.73335 1.77513 1.4 5.85559 1.4 7.61111C1.4 10.9368 1 14.221 1 17.5159C1 19.4153 1 21.3148 1 23.2143C1 25.2699 2.14463 25.5 5 25.5"
+        d="M4.2 1C0.73335 1.2522 1.4 4.99263 1.4 6.60185C1.4 9.65041 1 12.6609 1 15.6812V20.9048C1 22.7891 2.14463 23 5 23"
         stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
   </svg>
 </template>
