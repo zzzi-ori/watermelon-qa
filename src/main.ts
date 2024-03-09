bdbffad74e2b11b34b705dd2b7bc319cc31d67a7
da-in <talown@naver.com>
2024-03-09 15:23:43 +0900

    add user store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.mount('#app')
