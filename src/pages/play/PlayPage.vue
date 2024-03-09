a600b67a0b0677cac33446410fd11032ec209f2f
da-in <da-in@wavve.com>
2024-03-09 15:46:32 +0900

    add send home when no nickname

--- src/pages/play/PlayPage.vue
+++ src/pages/play/PlayPage.vue
@@ -16,7 +16,7 @@
   <game-over v-if="gameOverRef" :score="score" @replay="onReplay"/>
 </template>
 <script setup lang="ts">
-import {ref} from 'vue'
+import {onMounted, ref} from 'vue'
 import NextBlock from './_components/NextBlock.vue'
 import GameOver from './_components/GameOver.vue'
 import Ground from './_components/Ground.vue'
@@ -32,6 +32,12 @@ const userStore = useUserStore()
 const router = useRouter()
 const canvas = ref<HTMLCanvasElement>()
 const {score, replay, groundHeight, nextBlockRef, gameOverRef} = usePlayer(canvas)
+
+onMounted(() => {
+  if (!userStore.nickName) {
+    router.replace('/')
+  }
+})
 
 // todo canvas 미지원 브라우저에 대하여 matter.js 대응 체크
 // todo matter.js 하위 브라우저 대응 없을 시 canvas.getContext 함수 유무로 처리
