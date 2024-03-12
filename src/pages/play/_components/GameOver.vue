7c9cd80f2446a5b23403e51bb13335a7af7d4655
da-in <da-in@wavve.com>
2024-03-12 12:49:40 +0900

    update gameover ui

--- src/pages/play/_components/GameOver.vue
+++ src/pages/play/_components/GameOver.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed inset-0 flex items-center justify-center z-50 bg-transBlack-50">
+  <div class="absolute inset-0 flex items-center justify-center z-50 bg-transBlack-50 px-6">
     <div class="bg-white flex flex-col gap-6 px-6 py-6 justify-center items-center rounded-2xl border-2 border-black">
       <img :src="gameOver" alt="game over"/>
       <div class="text-body-b px-2 border-2 rounded">
@@ -9,7 +9,10 @@
       <div class="w-full text-body-r">
         <div class="w-full flex justify-between">
           <span>점수</span>
-          <span class="text-body-b">{{ score }}</span>
+          <div class="text-body-b flex gap-1">
+            <span>{{ score }}</span>
+            <img :src="coinSm" alt="코인"/>
+          </div>
         </div>
         <div class="w-full flex justify-between">
           <span>순위</span>
@@ -17,18 +20,25 @@
         </div>
       </div>
       <div class="flex gap-4">
-        <ZRoundButton :color="'secondary'" @click="$emit('back')">홈</ZRoundButton>
-        <ZRoundButton :color="'primary'" @click="$emit('replay')">다시</ZRoundButton>
+        <ZRoundButton :color="'secondary'" @click="$emit('back')" title="처음으로">
+          <img :src="home" alt="홈"/>
+        </ZRoundButton>
+        <ZRoundButton :color="'primary'" @click="$emit('replay')" title="다시하기">
+          <img :src="replay" alt="다시하기"/>
+        </ZRoundButton>
       </div>
-      <img class="max-w-72" :src="rankBanner" alt="실시간 랭킹 바로가기"/>
+      <img class="max-w-72" :src="rankBanner" alt="실시간 랭킹 바로가기" @click="$emit('rank')"/>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import gameOver from '../../../assets/game-over.svg'
+import replay from '../../../assets/replay.svg'
+import home from '../../../assets/home.svg'
 import gameOverZzio from '../../../assets/game-over-zzio.svg'
 import ZRoundButton from '../../../components/button/ZRoundButton.vue'
 import rankBanner from '../../../assets/rank-banner.png'
+import coinSm from '../../../assets/coin-sm.svg'
 
 defineProps({
   nickname: {
