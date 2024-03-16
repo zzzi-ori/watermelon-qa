cc16a5e0b7de298b49b32bf847d5349e0ee4f443
da-in <da-in@wavve.com>
2024-03-16 16:38:24 +0900

    add no rank error

--- src/pages/rank/RankPage.vue
+++ src/pages/rank/RankPage.vue
@@ -5,7 +5,12 @@
     </template>
     <img :src="rank" alt="명예의 찌오당"/>
   </ZHeader>
-  <div class="h-full overflow-y-scroll flex flex-col items-center">
+  <div v-if="list?.length === 0" class="w-full h-full flex flex-col items-center justify-center text-dark-green">
+    <img :src="error" alt="error"/>
+    <span class="text-body-b mb-1 mt-4">앗! 아직 참여한 사람이 없찌오</span>
+    <span class="text-caption-r">지금 플레이해서 1등을 차지하세요!</span>
+  </div>
+  <div v-else class="h-full overflow-y-scroll flex flex-col items-center">
     <span class="text-body-b mt-6 mb-4 text-dark-green">
       {{ totalCountRef }}명의 찌오가 함께했찌오
     </span>
@@ -26,7 +31,6 @@
 import rank from '../../assets/rank.svg'
 import ZHeader from '../../components/ZHeader.vue'
 import BackButton from '../../components/button/BackButton.vue'
-
 import {useRouter} from 'vue-router'
 import ZLabel from '../../components/ZLabel.vue'
 import medalZzio from '../../assets/medal-zzio.svg'
@@ -36,6 +40,7 @@ import {useGetInfiniteRank} from '../../requests/use/useGetInfiniteRank.ts'
 import {computed, ref} from 'vue'
 import ZRoundButton from '../../components/button/ZRoundButton.vue'
 import back from '../../assets/back.svg'
+import error from '../../assets/error.svg'
 
 const router = useRouter()
 const {data, fetchNextPage} = useGetInfiniteRank()
