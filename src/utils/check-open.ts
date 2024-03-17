49b355c0c4eb54d7ebc2275054e6fba5da3fabb2
da-in <da-in@wavve.com>
2024-03-17 16:40:37 +0900

    update service close date

--- src/utils/check-open.ts
+++ src/utils/check-open.ts
@@ -1,5 +1,5 @@
 export const isOpen = () => {
-  const targetTime = new Date('2024-03-17T19:00:00+09:00')
+  const targetTime = new Date('2024-03-18T19:00:00+09:00')
   const current = new Date()
   return current < targetTime
 }
