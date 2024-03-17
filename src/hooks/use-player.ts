aa3968abce32024afa0c38597d3eb016d7c2a645
da-in <talown@naver.com>
2024-03-17 16:41:26 +0900

    remove test code

--- src/hooks/use-player.ts
+++ src/hooks/use-player.ts
@@ -116,7 +116,7 @@ export const usePlayer = (element: Ref<HTMLCanvasElement | undefined>) => {
     })
   })
 
-  const {start, reset, count} = useTimer(3, () => {
+  const {start, reset} = useTimer(3, () => {
     endGame()
   })
 
@@ -182,5 +182,5 @@ export const usePlayer = (element: Ref<HTMLCanvasElement | undefined>) => {
     addBlock()
   }
 
-  return {engine, runner, score: scoreRef, collisions, drop, groundHeight, nextBlockRef, gameOverRef, replay, count}
+  return {engine, runner, score: scoreRef, collisions, drop, groundHeight, nextBlockRef, gameOverRef, replay}
 }
