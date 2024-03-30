d2bf4bc57e703eea3eddda450202084fe3189114
da-in <talown@naver.com>
2024-03-30 14:00:45 +0900

    add loading

---
+++ src/components/ZLoading.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="loader"></span>
+</template>
+<style scoped>
+.loader {
+  width: 16px;
+  height: 16px;
+  border: 2px solid #489B6D;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
