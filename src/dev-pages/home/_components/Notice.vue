5c17576ee6b712712bc8b30feaa234196b31dda3
da-in <da-in@wavve.com>
2024-03-30 14:00:56 +0900

    update dev-home

--- src/dev-pages/home/_components/Notice.vue
+++ src/dev-pages/home/_components/Notice.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="text-dark-green mt-6 mb-2">
-    <img :src="beta" class="inline mr-1" alt="beta"/>
-    <h3 class="text-caption-b inline">{{ titleRef }}</h3>
+    <img :src="event" class="inline mr-1" alt="beta"/>
+    <h3 class="text-caption-b inline">운영 안내</h3>
   </div>
   <ul class="text-caption-r text-dark-green list-disc ml-4">
     <li v-for="text in textRef" v-bind:key="text">{{ text }}</li>
   </ul>
 </template>
 <script setup lang="ts">
-import beta from '@/assets/beta.svg'
+import event from '@/assets/event.svg'
 import {computed} from 'vue'
 
 defineProps({
   closed: {type: Boolean, default: false},
 })
 
-const titleRef = computed(() => closed ? '운영 종료 안내' : '운영 안내')
+// todo: 내용 업데이트
 const textRef = computed(() => closed ?
   ['현재 찌오 게임 베타 버전은 종료 되었으며, 정식 버전은 24.03.24(일) 이후 오픈 예정입니다.'] :
   ['베타 버전은 24.03.18 (월) 19시 까지만 시범 운영 예정이며, 정식 버전은 24.03.24 (일) 이후 오픈 예정입니다.',
