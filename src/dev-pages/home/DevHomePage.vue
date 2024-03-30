049323303ed9b8c4415eba07e50db10ff7f600f3
da-in <talown@naver.com>
2024-03-30 14:00:56 +0900

    update dev-home

--- src/dev-pages/home/DevHomePage.vue
+++ src/dev-pages/home/DevHomePage.vue
@@ -3,32 +3,28 @@
     <img :src="zzioGame" alt="zzio game"/>
     <img :src="title" alt="황금 찌오를 찾아라 beta" class="my-8"/>
     <img :src="illustration" alt="찌오 이미지" class="mb-6"/>
-    <div v-if="closedRef" class="flex flex-col items-center w-full">
-      <ZButton color="secondary" :disabled="true">4월 초 오픈 예정</ZButton>
-    </div>
-    <div v-else class="flex flex-col items-center w-full">
-      <ZInput v-model="userStore.nickName"/>
-      <ZButton @click="onClickPlay" color="primary" :disabled="!userStore.nickName" class="mt-2">게임 시작</ZButton>
-      <ZButton
-          @click="onClickRank"
-          color="secondary"
-          :disabled="false"
-          class="mt-6"
-      >
-        실시간 랭킹
-      </ZButton>
-    </div>
-    <Notice :closed="closedRef"/>
+    <ZInput v-model="userStore.nickName"/>
+    <ZButton @click="onClickPlay" color="primary" :disabled="!userStore.nickName" class="mt-2">게임 시작</ZButton>
+    <ZButton
+        v-if="isOpenRef"
+        @click="onClickRank"
+        color="secondary"
+        :disabled="false"
+        class="mt-6"
+    >
+      실시간 랭킹
+    </ZButton>
+    <Notice :closed="!isOpenRef"/>
   </div>
   <Footer/>
 </template>
 <script setup lang="ts">
 import zzioGame from '../../assets/zzio-game.svg'
-import title from '../../assets/title-beta.svg'
+import title from '../../assets/title-event.svg'
 import illustration from '../../assets/illustration.svg'
 import ZButton from '../../components/button/ZButton.vue'
 import Footer from '../../components/Footer.vue'
-import {ref} from 'vue'
+import {computed, onBeforeUnmount, ref} from 'vue'
 import Notice from './_components/Notice.vue'
 import ZInput from '../../components/ZInput.vue'
 import {useRouter} from 'vue-router'
@@ -37,12 +33,20 @@ import {useUserStore} from '../../stores/user.ts'
 const router = useRouter()
 const userStore = useUserStore()
 
-const closedRef = ref(false)
-// const interval = setInterval(() => {
-//   closedRef.value = !isOpen()
-// }, 1000)
+const currentRef = ref<Date>(new Date())
+const isOpenRef = computed<boolean | undefined>(() => isOpen(currentRef.value))
+const closeTime = new Date('2024-04-19T23:59:59+09:00')
 
-// onBeforeUnmount(() => clearInterval(interval))
+const isOpen = (current: Date) => {
+  return current < closeTime
+}
+
+const interval = setInterval(() => {
+  currentRef.value = new Date()
+}, 1000)
+
+
+onBeforeUnmount(() => clearInterval(interval))
 
 const onClickPlay = () => {
   router.push('/play')
