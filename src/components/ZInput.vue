130545b966c62b9c9d2ec18e5abb1a32fa22c2b7
3people <smsn7031@gmail.com>
2024-04-05 01:46:35 +0900

    feat: event banner

--- src/components/ZInput.vue
+++ src/components/ZInput.vue
@@ -1,41 +1,41 @@
 <template>
   <input
-      :value="modelValue"
-      @input="onInupt"
-      placeholder="닉네임을 입력하세요"
-      :class="style({filled})"
-      :maxlength="max"
-  >
+    :value="modelValue"
+    @input="onInput"
+    :placeholder="placeholder"
+    :class="style({ filled })"
+    :maxlength="max"
+  />
 </template>
 <script setup lang="ts">
-
-import {computed} from 'vue'
-import {cva} from 'class-variance-authority'
+import { computed } from 'vue'
+import { cva } from 'class-variance-authority'
 
 const props = defineProps({
-  max: {type: Number, default: 8},
-  modelValue: {type: String, default: ''}
+  max: { type: Number, default: 8 },
+  modelValue: { type: String, default: '' },
+  placeholder: { type: String, default: '닉네임을 입력하세요' },
 })
 
 const emit = defineEmits(['update:model-value'])
 
 const filled = computed(() => props.modelValue.length > 0)
 
-const onInupt = (event: Event) => {
+const onInput = (event: Event) => {
   const target = event.target as HTMLInputElement
-  target.value = target.value.replace(/^\s*/, '').slice(0, 8)
+  target.value = target.value.replace(/^\s*/, '').slice(0, props.max)
   emit('update:model-value', target.value)
 }
 
-const style = cva([
-  'w-full text-body-b text-center p-4',
-  'rounded-[8px] px-[16px] py-[12px] border-2'
-], {
-  variants: {
-    filled: {
-      true: '',
-      false: 'border-dark-green'
-    }
-  }
-})
+const style = cva(
+  ['w-full text-body-b text-center p-4', 'rounded-[8px] px-[16px] py-[12px] border-2'],
+  {
+    variants: {
+      filled: {
+        true: '',
+        false: 'border-dark-green',
+      },
+    },
+  },
+)
 </script>
