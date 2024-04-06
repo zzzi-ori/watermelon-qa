d8454ab376e552660aa5e5d8d9be718aae21b19a
3people <smsn7031@gmail.com>
2024-04-06 18:31:25 +0900

    fix: rollback block

--- src/utils/get-block-index.ts
+++ src/utils/get-block-index.ts
@@ -2,8 +2,6 @@
  * @param count 몇 번째 블럭인지
  */
 export const getBlockIndex = (count: number) => {
-  // const _count = count > 6 ? 6 : count
-  console.log(count)
-  // return Math.floor(Math.random() * (_count - 1)) + 1 // 1 ~ count
-  return 9
+  const _count = count > 6 ? 6 : count
+  return Math.floor(Math.random() * (_count - 1)) + 1 // 1 ~ count
 }
