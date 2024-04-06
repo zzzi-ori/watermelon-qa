968f00a30a5330b61175e864fd9ce133e0b1dc54
da-in <talown@naver.com>
2024-04-06 16:00:54 +0900

    add obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {resolve} from 'path'
+import { resolve } from 'path'
+import obfuscator from 'rollup-plugin-obfuscator'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
+    rollupOptions: {
+      output: {
+        plugins: [
+          obfuscator(),
+        ],
+      },
+    },
     sourcemap: true,
   },
   base: '/',
   resolve: {
-    alias: [{find: '@', replacement: resolve(__dirname, './src')}]
-  }
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+  },
 })
