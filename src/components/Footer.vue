4cd52e2f721f2a66e39ccf5c812745d5542ff81a
da-in <talown@naver.com>
2024-04-06 16:05:05 +0900

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="text-dark-green flex flex-col items-center pb-12 pt-4">
     <a class="flex items-center gap-1 text-caption-b" href="https://github.com/zzzi-ori" target="_blank">
-      <img class="text-dark-green" :src="github" alt="github"/>
+      <img class="text-dark-green" :src="github" alt="github" />
       <h3>zzio-game</h3>
     </a>
-    <span class="text-caption-r">© developers are maybe ok</span>
+    <span class="text-caption-r">© IT’S YOUR PRIMETIME</span>
+    <span class="text-caption-r">develop by zzio-games</span>
   </div>
 </template>
 <script setup lang="ts">
