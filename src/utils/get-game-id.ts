6f44ecd5b97527ecec99e7789dfa3820683a20c9
da-in <talown@naver.com>
2024-04-06 18:52:36 +0900

    update game-id key

--- src/utils/get-game-id.ts
+++ src/utils/get-game-id.ts
@@ -3,6 +3,6 @@ import * as crypto from 'crypto-js'
 export const getGameId = () => {
   const current = new Date()
   const text = JSON.stringify(current)
-  const encrypted = crypto.AES.encrypt(text, 'zzio-games-watermelon').toString()
+  const encrypted = crypto.AES.encrypt(text, 'zzio-games-water').toString()
   return encrypted
 }
