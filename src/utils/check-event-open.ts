64daa8592dd4ac01409a57e3d632da04815c16c9
da-in <talown@naver.com>
2024-04-06 18:44:38 +0900

    remove event end test

--- src/utils/check-event-open.ts
+++ src/utils/check-event-open.ts
@@ -1,5 +1,4 @@
-// export const targetTime = new Date('2024-04-19T23:59:59+09:00')
-export const targetTime = new Date('2024-04-06T18:39:59+09:00')
+export const targetTime = new Date('2024-04-19T23:59:59+09:00')
 
 export const isEventOpen = () => {
   const current = new Date()
