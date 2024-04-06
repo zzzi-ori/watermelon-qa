2076f0b3bc3cf4bb475111fe8434fb00b11f572c
da-in <talown@naver.com>
2024-04-06 16:23:05 +0900

    remove obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import obfuscator from 'rollup-plugin-obfuscator'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
-    rollupOptions: {
-      output: {
-        plugins: [
-          obfuscator(),
-        ],
-      },
-    },
     sourcemap: true,
   },
   base: '/',
