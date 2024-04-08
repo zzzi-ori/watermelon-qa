9ad544b16f5385a8715ad90cbc29b17319221770
da-in <talown@naver.com>
2024-04-09 07:03:19 +0900

    close maintenance

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import PlayPage from '../pages/play/PlayPage.vue'
-// import RankPage from '../pages/rank/RankPage.vue'
-// import DevHomePage from '../event-pages/home/DevHomePage.vue'
-import MaintenancePage from '@/event-pages/home/MaintenancePage.vue'
+import PlayPage from '../pages/play/PlayPage.vue'
+import RankPage from '../pages/rank/RankPage.vue'
+import DevHomePage from '../event-pages/home/DevHomePage.vue'
+// import MaintenancePage from '@/event-pages/home/MaintenancePage.vue'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   component: DevHomePage,
-  // },
   {
     path: '/',
-    component: MaintenancePage,
+    component: DevHomePage,
+  },
+  {
+    path: '/play',
+    component: PlayPage,
+  },
+  {
+    path: '/rank',
+    component: RankPage,
   },
   // {
-  //   path: '/play',
-  //   component: PlayPage,
-  // },
-  // {
-  //   path: '/rank',
-  //   component: RankPage,
+  //   path: '/',
+  //   component: MaintenancePage,
   // },
 ]
 
@@ -28,10 +28,10 @@ const router = createRouter({
   routes,
 })
 
-router.beforeEach((to) => {
-  if (to.fullPath !== '/') {
-    return { path: '/' }
-  }
-})
+// router.beforeEach((to) => {
+//   if (to.fullPath !== '/') {
+//     return { path: '/' }
+//   }
+// })
 
 export default router
