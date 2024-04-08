5d1a078b2bedd138cf1a4a3bb34fe1ce1d3408d1
da-in <talown@naver.com>
2024-04-08 13:22:13 +0900

    remove source map

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    sourcemap: true,
-  },
   base: '/',
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
